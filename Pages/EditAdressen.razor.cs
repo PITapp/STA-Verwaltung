f87f0458fde447f22cf496aa2668b3996bf942c7
Painsi Günther <g.painsi@pitapp.at>
2023-11-11 17:31:59 +0100

    2023-11-11 17:31 gp

---
+++ Pages/EditAdressen.razor.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace STAVerwaltung.Pages
+{
+    public partial class EditAdressen
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+        [Inject]
+        public dbSTAVerwaltungService dbSTAVerwaltungService { get; set; }
+
+        [Parameter]
+        public int AdressNr { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            adressen = await dbSTAVerwaltungService.GetAdressenByAdressNr(AdressNr);
+
+            adressenArtenForAdressArt = await dbSTAVerwaltungService.GetAdressenArten();
+
+            adressenAnredenForAnredeCode = await dbSTAVerwaltungService.GetAdressenAnreden();
+
+            bundeslaenderForBundeslandCode = await dbSTAVerwaltungService.GetBundeslaender();
+
+            adressenFamilienstaendeForFamilienstandCode = await dbSTAVerwaltungService.GetAdressenFamilienstaende();
+
+            gemeindenForGemeindeNr = await dbSTAVerwaltungService.GetGemeinden();
+
+            adressenGeschlechterForGeschlecht = await dbSTAVerwaltungService.GetAdressenGeschlechter();
+
+            lKZForLKZ = await dbSTAVerwaltungService.GetLKZ();
+
+            adressenSortierungFamilieForSortierungFamilie = await dbSTAVerwaltungService.GetAdressenSortierungFamilie();
+        }
+        protected bool errorVisible;
+        protected STAVerwaltung.Models.dbSTAVerwaltung.Adressen adressen;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.AdressenArten> adressenArtenForAdressArt;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.AdressenAnreden> adressenAnredenForAnredeCode;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.Bundeslaender> bundeslaenderForBundeslandCode;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.AdressenFamilienstaende> adressenFamilienstaendeForFamilienstandCode;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.Gemeinden> gemeindenForGemeindeNr;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.AdressenGeschlechter> adressenGeschlechterForGeschlecht;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.LKZ> lKZForLKZ;
+
+        protected IEnumerable<STAVerwaltung.Models.dbSTAVerwaltung.AdressenSortierungFamilie> adressenSortierungFamilieForSortierungFamilie;
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        protected async Task FormSubmit()
+        {
+            try
+            {
+                await dbSTAVerwaltungService.UpdateAdressen(AdressNr, adressen);
+                DialogService.Close(adressen);
+            }
+            catch (Exception ex)
+            {
+                errorVisible = true;
+            }
+        }
+
+        protected async Task CancelButtonClick(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
